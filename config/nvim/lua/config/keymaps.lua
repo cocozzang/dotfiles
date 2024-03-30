7f412f1d9cff804a8002715cc4433c4eaa0d7412
cocozzang <cocozz503@naver.com>
2024-03-31 08:55:51 +0900

    feat: add scrollEOF.nvim & keymap for jumping to eol in insert mode

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,4 @@
 -- add undo break-points
 vim.keymap.set({ "n", "i", "v" }, "<C-z>", vim.cmd.undo, { desc = "Undo" })
 vim.keymap.set("n", "<leader><CR>", "o<Esc>", { desc = "new line in nomal mode" })
+vim.keymap.set("i", "<A-a>", "<Esc>A", { desc = "jump to end of line in insert mode" })
