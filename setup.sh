ebd9ec8d4a3af60901f71e0c761efebbf7c011f0
cocozzang <cocozz503@naver.com>
2024-11-22 06:03:41 +0900

    setup symlink setting먼저 처리

--- setup.sh
+++ setup.sh
@@ -6,6 +6,18 @@
 # 3. zsh setting
 # 4. gen ssh gpg
 
+delete_prev_conf() {
+  for file in "${prev_conf[@]}"; do
+    rm "~/$file"
+  done
+}
+
+setup_symlink() {
+  delete_prev_conf
+  cd ~/dotfiles/config
+  stow -t $HOME .
+}
+
 declare -a common_packages=(
   git lazygit zsh curl wget fzf fd ripgrep tree xclip ca-certificates gnupg less python python3 htop nodejs-lts-iron npm
   neofetch openssh rsync avahi reflector trash-cli clang cmake zip unzip docker docker-compose lua lua51 luarocks stow zoxide
@@ -17,7 +29,7 @@ install_common_packages() {
 
 # yay
 declare -a yay_packages=(
-  oh-my-posh
+  oh-my-posh vim-plug
 )
 
 install_yay() {
@@ -27,26 +39,19 @@ install_yay() {
   cd yay
   makepkg -si
   cd ~
-  rm -fr yay
 }
 
 declare -a yay_packages=(
-  oh-my-posh
+  oh-my-posh vim-plug
 )
 
 install_yay_packages() {
-  sudo yay -S --needed "${yay_packages[@]}"
+  yay -S --needed "${yay_packages[@]}"
 }
 
 declare -a prev_conf=(
   ".bashrc" ".bash_profile" ".bash_logout" ".bash_history"
 )
-
-delete_prev_conf() {
-  for file in "${prev_conf[@]}"; do
-    rm "~/$file"
-  done
-}
 
 install_packages() {
   install_common_packages
@@ -54,21 +59,15 @@ install_packages() {
   install_yay_packages
 }
 
-setup_symlink() {
-  delete_prev_conf
-  cd ~/dotfiles/config
-  stow -t $HOME .
-}
-
 # generate ssh for github
-# clone한 repo가 push되지않는다면 등록된 원격 rpeo를 삭제후 다시 등록하세요.
+# http로 clone한 repo가 push되지않는다면 등록된 원격 rpeo를 삭제후 ssh로 다시 등록하세요.
 # https://docs.github.com/ko/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 gen_ssh() {
   ssh-keygen -t rsa -b 4096 -C "cocozz503@naver.com"
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
   cat ~/.ssh/id_rsa.pub
-  echo ~/.ssh/id_rsa.pub | xclip -sel clip
+  cat ~/.ssh/id_rsa.pub | xclip -sel clip
   echo -e "\e[2;30;103m ssh 공개키가 클립보드에 복사되었습니다. github에 등록하세요 \e[0m \n"
   read -p "등록이 완료되었으면 Enter를 눌러 계속"
 }
@@ -77,10 +76,12 @@ gen_ssh() {
 gen_gpg() {
   gpg --full-generate-key # RSA & RSA / 4096 / no expire / cocozzang / cocozz503@naver.com
   gpg --list-secret-keys --keyid-format=long
-  # sec   4096R/3AA5C34371567BD2 2016-03-10 [expires: 2017-03-10] 에서 3AA5C34371567BD2 부분이 key입니다
-  echo "gpg --armor --export [key] 를 입력하고 해당 출력을 github에 등록하세요."
+  echo "sec   4096R/3AA5C34371567BD2 2016-03-10 [expires: 2017-03-10] 에서 3AA5C34371567BD2 부분이 key입니다"
+  echo "새로 터미널 창을열고 gpg --armor --export [key] 를 입력하고 해당 출력을 github에 등록하세요."
   echo "git config --global user.signingkey [key]로 gitconfig파일에 public gpg key추가하기"
   read -p "등록이 완료되었으면 Enter를 눌러 계속"
+  echo "hi coco" >>~/.gnupg/coco.txt
+  gpg --encrypt --recipient cocozz503@naver.com ~/.gnupg/coco.txt
 }
 
 # https://github.com/bfrg/gpg-guide/blob/master/gpg-agent.conf
@@ -106,15 +107,16 @@ nerd_font_info() {
 \e[2;30;103m window terminal emulator내의 Arch profile font를 CaskaydiaCove Nerd Font를 로 바꿔 \e[0m \n"
 
   read -p "Nerd font 적용이 완료되었으면 Enter를 눌러 계속진행하기"
+
+  echo "현재 유저 docker group에 추가"
   sudo usermod -aG docker $USER
-  cd ~
-  chsh $USER
-  /bin/zsh
+
+  echo -e "chsh \$USER로 현재 유저의 쉘을 zsh로 바꾸고 wsl을 재시작하세요"
 }
 
 setup_dotfiles() {
-  install_packages
   setup_symlink
+  install_packages
   gen_secret
   nerd_font_info
 }
