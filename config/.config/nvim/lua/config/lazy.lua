022fbdb2679efd12cf57b5eb2dc661d94ac1333d
cocozzang <cocozz503@naver.com>
2025-03-24 14:54:50 +0900

    disable smoth scroll & add scrollEOF

--- config/.config/nvim/lua/config/lazy.lua
+++ config/.config/nvim/lua/config/lazy.lua
@@ -32,6 +32,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "plugins" },
