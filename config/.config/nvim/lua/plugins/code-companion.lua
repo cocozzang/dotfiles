dc666588b2f36b4d85537581826cb90cc46a553a
cocozzang <cocozz503@naver.com>
2025-04-08 11:37:33 +0900

    add gemini2.5 to code compainon

--- config/.config/nvim/lua/plugins/code-companion.lua
+++ config/.config/nvim/lua/plugins/code-companion.lua
@@ -11,19 +11,31 @@ return {
       },
       strategies = {
         chat = {
-          adapter = "anthropic",
+          adapter = "gemini",
           tools = {
-            ["mcp"] = {
-              callback = require("mcphub.extensions.codecompanion"),
-              description = "Call tools and resources from the MCP Servers",
-              opts = {
-                requires_approval = false,
+            groups = {
+              ["mcp"] = {
+                callback = require("mcphub.extensions.codecompanion"),
+                description = "Call tools and resources from the MCP Servers",
+                opts = {
+                  requires_approval = false,
+                },
+              },
+              ["cmd_runner"] = {
+                opts = {
+                  requires_approval = false,
+                },
+              },
+              ["files"] = {
+                opts = {
+                  requires_approval = false,
+                },
               },
             },
           },
         },
         inline = {
-          adapter = "anthropic",
+          adapter = "gemini",
         },
       },
       adapters = {
@@ -32,12 +44,34 @@ return {
             env = {
               api_key = os.getenv("ANTHROPIC_API_KEY"),
             },
+            schema = {
+              model = {
+                default = "claude-3-7-sonnet-20250219",
+              },
+            },
           })
         end,
         openai = function()
           return require("codecompanion.adapters").extend("openai", {
             env = {
               api_key = os.getenv("OPENAI_API_KEY"),
+            },
+            schema = {
+              model = {
+                default = "gpt-4o-mini",
+              },
+            },
+          })
+        end,
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = os.getenv("GEMINI_API_KEY"),
+            },
+            schema = {
+              model = {
+                default = "gemini-2.5-pro-exp-03-25",
+              },
             },
           })
         end,
